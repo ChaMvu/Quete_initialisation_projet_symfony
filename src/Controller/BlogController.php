47bc60cdfe513f82bee8edd1d9dbcadc9d12cce3
Wilder <charlene.mvu@hotmail.fr>
2019-05-20 15:51:58 +0200

    recuperation données

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,36 +8,115 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\ArticleRepository;
 use phpDocumentor\Reflection\Types\String_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+
+/**
+ * Class BlogController
+ * @package App\Controller
+ * @Route("/blog")
+ */
 
 class BlogController extends AbstractController
 {
     /**
-    * @Route("/blog", name="blog_index")
-    */
+     * Show all row from article's entity
+     *
+     * @Route("/", name="index")
+     * @return Response A response instance
+     */
 
-    public function index()
+    public function index(): Response
     {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
 
         return $this->render('Blog/index.html.twig', [
-                'owner' => 'Charlene',
+                'articles' => $articles,
             ]
         );
     }
 
     /**
-     * @Route("/blog/show/{article}", name="blog_show", methods={"GET"}, requirements={"article"="[a-z0-9-]+"})
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="blog_show")
+     *
+     * @return Response A response instance
      */
-    public function show(string $article = 'Article Sans Titre')
+
+    public function show(?string $slug) : Response
     {
-        $article= str_replace('-', ' ', $article);
-        $article = ucwords($article);
+
+        if (!$slug) {
+            throw $this
+            ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with'.$slug.'title, found in article\'s table.'
+            );
+        }
+
         return $this->render('Blog/show.html.twig', [
-            'title' => $article,
+            'article' => $article,
+            'slug' => $slug
             ]
         );
     }
+
+    /**
+     * @param string $categoryName
+     *
+     * @Route("/category/{categoryName}" , name = "show_category")
+     *
+     * @return Response A response instance
+     */
+
+    public function showByCategory(string $categoryName = 'Javascript'): Response
+    {
+        $data = [];
+        $data['category'] = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => mb_strtolower($categoryName)]);
+
+
+        $data['articles'] = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy(['category' => $data['category']], ['id' => 'DESC'], 3);
+
+
+        return $this->render(
+            'Blog/category.html.twig',
+            $data
+        );
+    }
+
+
 }
---
+++ var/cache/dev/twig/78/78f4505f28cec88544640aa2fbc580c3338912e1ad5098a12a38b91308d46035.php
@@ -0,0 +1,135 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* Blog/category.html.twig */
+class __TwigTemplate_ec40b391650633a3a6f9fd8077ad899f961e252da9e0903d7319d9d4b4f2999b extends \Twig\Template
+{
+    private $source;
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "Blog/category.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "Blog/category.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "Blog/category.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 4
+    public function block_body($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 5
+        echo "    ";
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["category"]) || array_key_exists("category", $context) ? $context["category"] : (function () { throw new RuntimeError('Variable "category" does not exist.', 5, $this->source); })()), "name", [], "any", false, false, false, 5), "html", null, true);
+        echo "
+    ";
+        // line 6
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["articles"]) || array_key_exists("articles", $context) ? $context["articles"] : (function () { throw new RuntimeError('Variable "articles" does not exist.', 6, $this->source); })()));
+        foreach ($context['_seq'] as $context["_key"] => $context["article"]) {
+            // line 7
+            echo "
+        <h1>";
+            // line 8
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["article"], "id", [], "any", false, false, false, 8), "html", null, true);
+            echo " ";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["article"], "title", [], "any", false, false, false, 8), "html", null, true);
+            echo "</h1>
+
+        <p>";
+            // line 10
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["article"], "content", [], "any", false, false, false, 10), "html", null, true);
+            echo "</p>
+
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['article'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 13
+        echo "
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "Blog/category.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  93 => 13,  84 => 10,  77 => 8,  74 => 7,  70 => 6,  65 => 5,  56 => 4,  34 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+
+{% block body %}
+    {{ category.name }}
+    {% for article in articles %}
+
+        <h1>{{ article.id }} {{ article.title }}</h1>
+
+        <p>{{ article.content }}</p>
+
+    {% endfor %}
+
+{% endblock %}", "Blog/category.html.twig", "/home/wilder/workspace/symfony/quete1/blog/templates/Blog/category.html.twig");
+    }
+}
